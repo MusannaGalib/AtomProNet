8b24154b9869c284e23aa0145fafaae7c1c19d1b
MusannaGalib <musannagalib2016@gmail.com>
2024-10-15 21:48:55 -0700

    AtomProNet

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='VASP_Neural_Network_dataset_converter',  # Name of the package
+    name='AtomProNet',  # Name of the package
     version='0.1.0',  # Version of the package
     author='Musanna Galib',  # Replace with your name
     author_email='galibubc@student.ubc.ca',  # Replace with your email
     description='A Python package for converting VASP data to extxyz/npz format.',  # Short description of your package
     long_description=open('README.md').read(),  # Long description read from the README.md
     long_description_content_type='text/markdown',  # Type of the long description, markdown is recommended
-    url='http://example.com/VASP_Neural_Network_dataset_converter',  # Replace with the URL to your package's repository
+    url='http://example.com/AtomProNet',  # Replace with the URL to your package's repository
     packages=find_packages(),  # Finds all the packages in your project
     install_requires=open('requirements.txt').read().splitlines(),  # List of dependencies, read from requirements.txt
     classifiers=[  # Classifiers give the PyPI users more information about your package
