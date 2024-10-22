adc4998b5686affc6c2a89badfd0074bf5febb98
MusannaGalib <musannagalib2016@gmail.com>
2024-10-21 23:32:21 -0700

    AtomProNet

--- scripts/hydrostatic_strain_submission.sh
+++ scripts/hydrostatic_strain_submission.sh
@@ -2,23 +2,23 @@
 
 #Submission script for hydrostatic strain
 # Initialize counter
-counter=0
+#counter=0
 
-for EXX in `seq -0.2 0.0001 0.2` # strain range in XX direction
-do 
-((counter++))
-	cd strain_XX_$EXX
-    EYY=$EXX
-    EZZ=$EXX
-  echo "Debug: Simulation $counter - EXX=$EXX, EYY=$EYY, EZZ=$EZZ"   
-	     cd strain_YY_$EYY
-    
-	          cd strain_ZZ_$EZZ
-                   sbatch vasp_job.sh
-	          cd ..	          
-	     cd ..	
-cd ..
-done
+#for EXX in `seq -0.2 0.0001 0.2` # strain range in XX direction
+#do 
+#((counter++))
+#	cd strain_XX_$EXX
+#    EYY=$EXX
+#    EZZ=$EXX
+#  echo "Debug: Simulation $counter - EXX=$EXX, EYY=$EYY, EZZ=$EZZ"   
+#	     cd strain_YY_$EYY
+#    
+#	          cd strain_ZZ_$EZZ
+#                   sbatch vasp_job.sh
+#	          cd ..	          
+#	     cd ..	
+#cd ..
+#done
  
 
 
